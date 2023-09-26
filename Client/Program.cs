9dff72091cf593d54d5b6d9b45c6cca664e221a2
ATPTECH\kerimo <kerimo@tradesoft.com.tr>
2023-09-26 13:01:47 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        // Sunucunun IP adresi ve port numarası
+        string serverIP = "127.0.0.1";
+        int serverPort = 12345;
+
+        TcpClient client = new TcpClient();
+        client.Connect(serverIP, serverPort);
+
+        NetworkStream stream = client.GetStream();
+
+        //string message = "Merhaba, TCP Sunucusu!";
+        //byte[] data = Encoding.ASCII.GetBytes(message);
+        //stream.Write(data, 0, data.Length);
+
+
+        var text = "aa";
+        byte[] data = Encoding.ASCII.GetBytes(text);
+        stream.Write(data, 0, data.Length);
+
+        Task.Run(() =>
+        {
+            dinle(stream);
+        });
+
+
+
+        //stream.Close();
+        //client.Close();
+
+        Console.Read();
+
+    }
+    static Task oku(NetworkStream stream)
+    {
+        while (true)
+        {
+            
+            
+         
+        }
+
+    }
+    static Task dinle(NetworkStream stream)
+    {
+        while (true)
+        {
+           
+                // Yanıtı al
+                byte[] responseData = new byte[1024];
+                int bytesRead = stream.Read(responseData, 0, responseData.Length);
+                string responseMessage = Encoding.ASCII.GetString(responseData, 0, bytesRead);
+                Console.WriteLine($"Alınan yanıt: {responseMessage}");
+         
+        }
+
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static string text;
+    static void Main()
+    {
+        // Sunucunun IP adresi ve port numarası
+        string serverIP = "127.0.0.1";
+        int serverPort = 12345;
+
+        TcpListener server = new TcpListener(IPAddress.Parse(serverIP), serverPort);
+        server.Start();
+
+        Console.WriteLine($"Sunucu {serverIP}:{serverPort} üzerinde dinleniyor...");
+
+        TcpClient client = server.AcceptTcpClient();
+        NetworkStream stream = client.GetStream();
+
+        byte[] data = new byte[1024];
+        int bytesRead = stream.Read(data, 0, data.Length);
+        string message = Encoding.ASCII.GetString(data, 0, bytesRead);
+        Console.WriteLine($"Alınan veri: {message}");
+
+        while (true)
+        {
+            var text = Console.ReadLine();
+
+            string responseMessage = $"{message} => {text}";
+            byte[] responseData = Encoding.ASCII.GetBytes(responseMessage);
+            stream.Write(responseData, 0, responseData.Length);
+        }
+
+        stream.Close();
+        client.Close();
+        server.Stop();
+
+        Console.Read();
+    }
+}
